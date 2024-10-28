f79f0307f57ed09dfead3036df78567a2675e554
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2024-10-28 15:10:35 +0530

    Add files via upload

---
+++ 5python(Monday).py
@@ -0,0 +1,33 @@
+'''import math
+base= int(input("enter base"))
+expo=int(input("enter expo"))
+p= math.pow(base,expo)
+print("{} power {} is :{}".format(base,expo,p))'''
+#a=5
+#print(type(a))
+#a=eval(input("enter  a value"))
+#b=eval(input("enter b value"))
+#c=a+b
+#print("Addition of {} and {} is{:.2f}".format(a,b,c))
+#import math
+#a,b,c=map(int,input( " enter values of a,b,c").split())
+#s=(a+b+c)/2
+#d=s*(s-a)*(s-b)*(s-c)
+#area=math.sqrt(d)
+#print("{:.2f}".format(area))
+'''import math
+r=int(input("enter the value of radius"))
+h=int(input("enter h value"))
+v=2*math.pi*r*h
+print( "{:.2f}".format(v))
+list=[1,2,3,4,5]
+print(list[0])'''
+a=100//60
+print("{:.0f} hours".format(a))
+b=100%60
+print("{:.0f} minutes".format(b))
+
+
+      
+
+
